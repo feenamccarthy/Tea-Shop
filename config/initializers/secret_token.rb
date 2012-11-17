d88920b5f7c716cfb05c718db36fa1eb046e1015
feenamccarthy <feenamccarthy@yahoo.com>
2012-11-17 10:27:38 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teashop::Application.config.secret_token = '4e8b35cdbd67dd57fcce59d10f120defe27403d82e3e55edbf3dd0c28be3d52a99a6b1fd64c22cccb708eb2939252ad8204c525b444c8065b1cc3aa10f154392'
